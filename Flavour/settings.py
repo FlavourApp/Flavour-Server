b1783f2725ea8a79997992b853a7954be575f8b2
ro3rto <ro3rto@gmail.com>
2015-03-03 22:58:22 -0300

    debug

--- Flavour/settings.py
+++ Flavour/settings.py
@@ -76,8 +76,8 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = 'flavourapp@gmail.com'
-EMAIL_HOST_PASSWORD = 'flavourappmail'
+EMAIL_HOST_USER = 'flavourappmail@gmail.com'
+EMAIL_HOST_PASSWORD = 'flavourapp'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
