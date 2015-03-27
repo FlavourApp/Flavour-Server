257c8739208823847460e1585a1b7897243c5278
ro3rto <ro3rto@github.com>
2015-03-26 22:07:15 -0400

    fixed untracked files

--- db/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from db.models import *
-
-class ReservaForm(forms.ModelForm):
-  class Meta:
-  	model = Reserva
-
-class ComunaForm(forms.Form):
-	comuna = forms.ModelChoiceField(queryset=Comuna.objects.all())
