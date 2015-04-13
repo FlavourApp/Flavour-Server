47019ea3ac7f68512dba0d61caaf211ab32ce051
root <ro3rto@gmail.com>
2015-04-13 22:41:58 +0000

    static files for admin

--- db/admin.py
+++ db/admin.py
@@ -1,20 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from db.models import User, Chef, Comuna, Menu, ChefBioFoodImage, MenuImage, Consumer, Date, Reserva
-
-
+from db.models import Chef, Comuna, Menu, Date, Reserva
 
 class ChefAdmin(admin.ModelAdmin):
 	readonly_fields = ('image_tag',)
 	fields = ['name', 'lastname', 'email', 'phone', 'picture', 'description', 'bio', 'comunas','image_tag']
 
-admin.site.register(User)
-admin.site.register(Chef, ChefAdmin)
+#admin.site.register(User)
+admin.site.register(Chef)
 admin.site.register(Comuna)
 admin.site.register(Menu)
-admin.site.register(ChefBioFoodImage)
-admin.site.register(MenuImage)
-admin.site.register(Consumer)
+#admin.site.register(ChefBioFoodImage)
+#admin.site.register(MenuImage)
+#admin.site.register(Consumer)
 admin.site.register(Date)
 admin.site.register(Reserva)
--- db/models.py
+++ db/models.py
@@ -3,9 +3,9 @@ import time
 
 # Create your models here.
 
-class User(models.Model):
-	name 	= models.CharField(max_length=200, default = '')
-	surName = models.CharField(max_length=200, default = '')
+#class User(models.Model):
+#	name 	= models.CharField(max_length=200, default = '')
+#	surName = models.CharField(max_length=200, default = '')
 
 class Comuna(models.Model):	
 	name 	= models.CharField(max_length=60, default = '', primary_key=True)
@@ -33,21 +33,22 @@ class Chef(models.Model):
 	def __unicode__(self):
 		return "{} {} | {}".format(self.name, self.lastname, self.description)
 
-class Consumer(models.Model):
-	name 		= models.CharField(max_length=60, default = '')
-	lastname 	= models.CharField(max_length=60, default = '')
-	address 	= models.CharField(max_length=60, default = '')
-	phone 		= models.CharField(max_length=12, default = '')
-	FBID 		= models.CharField(max_length=60, default = '', blank=True)
-	email 		= models.EmailField(max_length=60, default = '')
-	comuna 		= models.ForeignKey(Comuna)	
 
-class ChefBioFoodImage(models.Model):
-	url 	= models.URLField(max_length=200)
-	chef 	= models.ForeignKey(Chef)
+#class Consumer(models.Model):
+#	name 		= models.CharField(max_length=60, default = '')
+#	lastname 	= models.CharField(max_length=60, default = '')
+#	address 	= models.CharField(max_length=60, default = '')
+#	phone 		= models.CharField(max_length=12, default = '')
+#	FBID 		= models.CharField(max_length=60, default = '', blank=True)
+#	email 		= models.EmailField(max_length=60, default = '')
+#	comuna 		= models.ForeignKey(Comuna)	
 
-	def __unicode__(self):
-		return "{}".format(self.url)
+#class ChefBioFoodImage(models.Model):
+#	url 	= models.URLField(max_length=200)
+#	chef 	= models.ForeignKey(Chef)
+#
+#	def __unicode__(self):
+#		return "{}".format(self.url)
 
 class Menu(models.Model):
 	name 			= models.CharField(max_length=200, default='')
@@ -92,9 +93,9 @@ class Reserva(models.Model):
 
 
 
-class MenuImage(models.Model):
-	url 	= models.URLField(max_length=200)
-	menu 	= models.ForeignKey(Menu)
-
-	def __unicode__(self):
-		return "{}".format(self.url)
+#class MenuImage(models.Model):
+#	url 	= models.URLField(max_length=200)
+#	menu 	= models.ForeignKey(Menu)
+#
+#	def __unicode__(self):
+#		return "{}".format(self.url)
